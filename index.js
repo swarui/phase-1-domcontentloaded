0d8be99780585df3d7d86e993de525e37c075ffc
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-28 00:09:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    console.log("The DOM has loaded")
+    content();
+ })
+ 
+ const content = () => {
+     const textToShow = document.getElementById("text");
+     textToShow.textContent= "This is really cool!";
+ 
+ };
